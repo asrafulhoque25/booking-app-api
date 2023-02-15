aff7bd64ac5dbdac117d26f0667a3f626c848545
Asraful Hoque <96014879+asrafulhoque25@users.noreply.github.com>
2023-02-15 19:36:19 +0600

    create back end server, create model for users, create middle wire for authentication verification, create controller for authentication, create login route and registration route, create function for generic error message

---
+++ index.js
@@ -0,0 +1,67 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import authRoute from './routes/auth.js';
+import usersRoute from "./routes/users.js";
+
+
+const port = process.env.PORT || 5000 ;
+
+
+const app = express();
+dotenv.config();
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to mongoDB.");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+
+
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+
+
+
+// Routes 
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+
+
+
+// Error Handler 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Booking App Server is Running");
+});
+
+
+app.listen(port, () => {
+  connect();
+  console.log("Connected to backend.");
+});
